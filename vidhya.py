f9a938c2f73b22e1218f0e2a48d6216aa6957522
Mike DeLong <mikedelong@outlook.com>
2020-09-10 12:38:29 -0400

    feature: introduce get_relation and some code to exercise the whole pipeline. bug fix: clean up four comparisons

--- vidhya.py
+++ vidhya.py
@@ -13,55 +13,67 @@ from time import time
 import spacy
 import pandas as pd
 from tqdm import tqdm
+from spacy.matcher import Matcher
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 def get_entities(nlp, sent):
-    # chunk 1
-    ent1 = ""
-    ent2 = ""
+    ent1 = ''
+    ent2 = ''
 
-    prv_tok_dep = ""  # dependency tag of previous token in the sentence
-    prv_tok_text = ""  # previous token in the sentence
+    prv_tok_dep = ''  # dependency tag of previous token in the sentence
+    prv_tok_text = ''  # previous token in the sentence
 
-    prefix = ""
-    modifier = ""
+    prefix = ''
+    modifier = ''
 
     for tok in nlp(sent):
-        # chunk 2
         # if token is a punctuation mark then move on to the next token
-        if tok.dep_ != "punct":
+        if tok.dep_ != 'punct':
             # check: token is a compound word or not
-            if tok.dep_ == "compound":
+            if tok.dep_ == 'compound':
                 prefix = tok.text
                 # if the previous word was also a 'compound' then add the current word to it
-                if prv_tok_dep == "compound":
-                    prefix = prv_tok_text + " " + tok.text
+                if prv_tok_dep == 'compound':
+                    prefix = prv_tok_text + ' ' + tok.text
 
             # check: token is a modifier or not
-            if tok.dep_.endswith("mod"):
+            if tok.dep_.endswith('mod'):
                 modifier = tok.text
                 # if the previous word was also a 'compound' then add the current word to it
-                if prv_tok_dep == "compound":
-                    modifier = prv_tok_text + " " + tok.text
+                if prv_tok_dep == 'compound':
+                    modifier = prv_tok_text + ' ' + tok.text
 
-            # chunk 3
-            if tok.dep_.find("subj"):  #:
-                ent1 = modifier + " " + prefix + " " + tok.text
-                prefix = ""
-                modifier = ""
-                prv_tok_dep = ""
-                prv_tok_text = ""
+            if tok.dep_.find('subj') != -1:  # == True:
+                ent1 = modifier + ' ' + prefix + ' ' + tok.text
+                prefix = ''
+                modifier = ''
 
-                # chunk 4
-            if tok.dep_.find("obj"):  #:
-                ent2 = modifier + " " + prefix + " " + tok.text
+            if tok.dep_.find('obj') != -1:  # == True:
+                ent2 = modifier + ' ' + prefix + ' ' + tok.text
 
-            # chunk 5  
             # update variables
             prv_tok_dep = tok.dep_
             prv_tok_text = tok.text
 
     return [ent1.strip(), ent2.strip()]
+
+
+def get_relation(nlp, sent):
+    doc = nlp(sent)
+    # Matcher class object
+    matcher = Matcher(nlp.vocab)
+    # define the pattern
+    pattern = [{'DEP': 'ROOT'},
+               {'DEP': 'prep', 'OP': '?'},
+               {'DEP': 'agent', 'OP': '?'},
+               {'POS': 'ADJ', 'OP': '?'}]
+    matcher.add('matching_1', None, pattern)
+    matches = matcher(doc)
+    k = len(matches) - 1
+    span = doc[matches[k][1]:matches[k][2]]
+    return span.text
 
 
 if __name__ == '__main__':
@@ -82,8 +94,26 @@ if __name__ == '__main__':
     df = pd.read_csv(filepath_or_buffer='./data/wiki_sentences_v2.csv')
     logger.info(df.shape)
 
-    model = spacy.load('en_core_web_sm')
+    model_name = 'en_core_web_sm'
+    model = spacy.load(model_name)
 
     pairs = [get_entities(model, item) for item in tqdm(df['sentence'])]
+    relations = [get_relation(model, item) for item in tqdm(df['sentence'])]
+    for pair in pairs:
+        logger.info('pair: {}'.format(pair))
+    for relation in relations:
+        logger.info('relation: {}'.format(relation))
+
+    result_df = pd.DataFrame({'source': [item[0] for item in pairs],
+                              'target': [item[1] for item in pairs], 'edge': relations})
+
+    if True:
+        graph = nx.from_pandas_edgelist(result_df[result_df['edge'] == 'composed by'], 'source', 'target',
+                                        edge_attr=True, create_using=nx.MultiDiGraph())
+
+        plt.figure(figsize=(12, 12))
+        pos = nx.spring_layout(graph, k=0.5)  # k regulates the distance between nodes
+        nx.draw(graph, with_labels=True, node_color='skyblue', node_size=1500, edge_cmap=plt.cm.Blues, pos=pos)
+        plt.show()
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
