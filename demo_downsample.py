2473c6d9ecd009ee19aadc341b487ecbfd5ad98b
Mike DeLong <mikedelong@outlook.com>
2020-10-02 13:22:29 -0400

    feature: some basic demo code using resample()

--- demo_downsample.py
+++ demo_downsample.py
@@ -7,6 +7,8 @@ from logging import getLogger
 from pathlib import Path
 from sys import stdout
 from time import time
+from scipy.sparse import random
+from sklearn.utils import resample
 
 if __name__ == '__main__':
     time_start = time()
@@ -21,5 +23,19 @@ if __name__ == '__main__':
     basicConfig(datefmt='%m-%d-%Y %H:%M:%S', format=format_, handlers=handlers_, level=log_level_, )
     logger = getLogger(__name__)
     logger.info('started')
+
+    m = 500
+    n = 500
+    density = 0.01
+    storage_format = 'coo'
+    random_state = 1
+    data_rvs = None
+    data = random(m=m, n=n, format=storage_format, random_state=random_state, data_rvs=data_rvs)
+    logger.info(data.nnz)
+
+    n_samples = 50
+    result = resample(data, n_samples=n_samples, random_state=random_state)
+
+    logger.info(result.nnz)
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
