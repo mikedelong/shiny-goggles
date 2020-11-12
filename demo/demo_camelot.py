389de7c6b71575bf41e143a8909b23e4c8518365
Mike DeLong <mikedelong@outlook.com>
2020-11-12 11:08:10 -0500

    feature: shell code with logging and total time

---
+++ demo/demo_camelot.py
@@ -0,0 +1,29 @@
+import datetime
+from logging import FileHandler
+from logging import INFO
+from logging import StreamHandler
+from logging import basicConfig
+from logging import getLogger
+from pathlib import Path
+from sys import stdout
+from time import time
+from json import load
+
+if __name__ == '__main__':
+    time_start = time()
+    LOG_PATH = Path('./logs/')
+    LOG_PATH.mkdir(exist_ok=True)
+    log_file = str(LOG_PATH / 'log-{}-{}.log'.format(datetime.datetime.today().strftime('%Y-%m-%d_%H-%M-%S'),
+                                                     'demo_camelot', ))
+    format_ = '%(asctime)s : %(levelname)s : %(name)s : %(message)s'
+    handlers_ = [FileHandler(log_file, encoding='utf-8', ), StreamHandler(stdout)]
+    log_level_ = INFO
+    # noinspection PyArgumentList
+    basicConfig(datefmt='%m-%d-%Y %H:%M:%S', format=format_, handlers=handlers_, level=log_level_, )
+    logger = getLogger(__name__)
+    logger.info('started')
+
+    with open(encoding='ascii', file='./settings.json', mode='r', ) as settings_fp:
+        settings = load(fp=settings_fp, )
+    logger.info(settings)
+    logger.info('total time: {:5.2f}s'.format(time() - time_start))
